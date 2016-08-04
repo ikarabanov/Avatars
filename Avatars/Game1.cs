4b8cb6e6aac40e6a7f91aec5e0f2efc21cec9b86
Ilya Karabanov <ilya.karabanov@ftrans.net>
2016-08-04 13:42:16 -0400

    Add project files.

---
+++ Avatars/Game1.cs
@@ -0,0 +1,132 @@
+﻿using Avatars.CharacterComponents;
+using Avatars.Components;
+using Avatars.GameStates;
+using Avatars.StateManager;
+using Avatars.TileEngine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Avatars
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Dictionary<AnimationKey, Animation> playerAnimations = new Dictionary<AnimationKey, Animation>();
+
+        GameStateManager gameStateManager;
+        CharacterManager characterManager;
+
+        ITitleIntroState titleIntroState;
+        IMainMenuState startMenuState;
+        IGamePlayState gamePlayState;
+
+        static Rectangle screenRectangle;
+
+        public SpriteBatch SpriteBatch
+        {
+            get { return spriteBatch; }
+        }
+
+        public static Rectangle ScreenRectangle
+        {
+            get { return screenRectangle; }
+        }
+
+        public ITitleIntroState TitleIntroState
+        {
+            get { return titleIntroState; }
+        }
+
+        public IMainMenuState StartMenuState
+        {
+            get { return startMenuState; }
+        }
+
+        public IGamePlayState GamePlayState
+        {
+            get { return gamePlayState; }
+        }
+
+        public Dictionary<AnimationKey, Animation> PlayerAnimations
+        {
+            get { return playerAnimations; }
+        }
+
+        public CharacterManager CharacterManager
+        {
+            get { return characterManager; }
+        }
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            screenRectangle = new Rectangle(0, 0, 1280, 720);
+
+            graphics.PreferredBackBufferWidth = ScreenRectangle.Width;
+            graphics.PreferredBackBufferHeight = ScreenRectangle.Height;
+
+            gameStateManager = new GameStateManager(this);
+            Components.Add(gameStateManager);
+
+            this.IsMouseVisible = true;
+
+            titleIntroState = new TitleIntroState(this);
+            startMenuState = new MainMenuState(this);
+            gamePlayState = new GamePlayState(this);
+            
+            gameStateManager.ChangeState((TitleIntroState)titleIntroState, PlayerIndex.One);
+
+            characterManager = CharacterManager.Instance;
+        }
+
+        protected override void Initialize()
+        {
+            Components.Add(new Xin(this));
+
+            Animation animation = new Animation(3, 64, 64, 0, 0);
+            playerAnimations.Add(AnimationKey.WalkDown, animation);
+
+            animation = new Animation(3, 64, 64, 0, 64);
+            playerAnimations.Add(AnimationKey.WalkLeft, animation);
+
+            animation = new Animation(3, 64, 64, 0, 128);
+            playerAnimations.Add(AnimationKey.WalkRight, animation);
+
+            animation = new Animation(3, 64, 64, 0, 192);
+            playerAnimations.Add(AnimationKey.WalkUp, animation);
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Avatars/GameStates/GamePlayState.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avatars.Components;
+using Avatars.TileEngine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Avatars.PlayerComponents;
+using Avatars.CharacterComponents;
+
+namespace Avatars.GameStates
+{
+    public interface IGamePlayState
+    {
+        void SetUpNewGame();
+        void LoadExistingGame();
+        void StartGame();
+    }
+
+    public class GamePlayState : BaseGameState, IGamePlayState
+    {
+        Engine engine = new Engine(Game1.ScreenRectangle, 64, 64);
+        TileMap map;
+        Camera camera;
+        Player player;
+
+        public GamePlayState(Game game)
+            : base(game)
+        {
+            game.Services.AddService(typeof(IGamePlayState), this);
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            Texture2D spriteSheet = content.Load<Texture2D>(@"PlayerSprites\maleplayer");
+            player = new Player(GameRef, "Wesley", false, spriteSheet);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Vector2 motion = Vector2.Zero;
+            int cp = 8;
+
+            if (Xin.KeyboardState.IsKeyDown(Keys.W) && Xin.KeyboardState.IsKeyDown(Keys.A))
+            {
+                motion.X = -1;
+                motion.Y = -1;
+                player.Sprite.CurrentAnimation = AnimationKey.WalkLeft;
+            }
+            else if (Xin.KeyboardState.IsKeyDown(Keys.W) && Xin.KeyboardState.IsKeyDown(Keys.D))
+            {
+                motion.X = 1;
+                motion.Y = -1;
+                player.Sprite.CurrentAnimation = AnimationKey.WalkRight;
+            }
+            else if (Xin.KeyboardState.IsKeyDown(Keys.S) && Xin.KeyboardState.IsKeyDown(Keys.A))
+            {
+                motion.X = -1;
+                motion.Y = 1;
+                player.Sprite.CurrentAnimation = AnimationKey.WalkLeft;
+            }
+            else if (Xin.KeyboardState.IsKeyDown(Keys.S) && Xin.KeyboardState.IsKeyDown(Keys.D))
+            {
+                motion.X = 1;
+                motion.Y = 1;
+                player.Sprite.CurrentAnimation = AnimationKey.WalkRight;
+            }
+            else if (Xin.KeyboardState.IsKeyDown(Keys.W))
+            {
+                motion.Y = -1;
+                player.Sprite.CurrentAnimation = AnimationKey.WalkUp;
+            }
+            else if (Xin.KeyboardState.IsKeyDown(Keys.S))
+            {
+                motion.Y = 1;
+                player.Sprite.CurrentAnimation = AnimationKey.WalkDown;
+            }
+            else if (Xin.KeyboardState.IsKeyDown(Keys.A))
+            {
+                motion.X = -1;
+                player.Sprite.CurrentAnimation = AnimationKey.WalkLeft;
+            }
+            else if (Xin.KeyboardState.IsKeyDown(Keys.D))
+            {
+                motion.X = 1;
+                player.Sprite.CurrentAnimation = AnimationKey.WalkRight;
+            }
+
+            if (motion != Vector2.Zero)
+            {
+                motion.Normalize();
+                motion *= (player.Speed * (float)gameTime.ElapsedGameTime.TotalSeconds);
+
+                Rectangle pRect = new Rectangle(
+                    (int)player.Sprite.Position.X + (int)motion.X + cp,
+                    (int)player.Sprite.Position.Y + (int)motion.Y + cp,
+                    Engine.TileWidth - cp,
+                    Engine.TileHeight - cp);
+
+                foreach (string s in map.Characters.Keys)
+                {
+                    ICharacter c = GameRef.CharacterManager.GetCharacter(s);
+                    Rectangle r = new Rectangle(
+                        (int)map.Characters[s].X * Engine.TileWidth + cp,
+                        (int)map.Characters[s].Y * Engine.TileHeight + cp,
+                        Engine.TileWidth - cp,
+                        Engine.TileHeight - cp);
+
+                    if (pRect.Intersects(r))
+                    {
+                        motion = Vector2.Zero;
+                        break;
+                    }
+                }
+
+                Vector2 newPosition = player.Sprite.Position + motion;
+
+                player.Sprite.Position = newPosition;
+                player.Sprite.IsAnimating = true;
+                player.Sprite.LockToMap(new Point(map.WidthInPixels, map.HeightInPixels));
+            }
+            else
+            {
+                player.Sprite.IsAnimating = false;
+            }
+
+            camera.LockToSprite(map, player.Sprite, Game1.ScreenRectangle);
+            player.Sprite.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            if (map != null && camera != null)
+                map.Draw(gameTime, GameRef.SpriteBatch, camera);
+
+            GameRef.SpriteBatch.Begin(
+                SpriteSortMode.Deferred,
+                BlendState.AlphaBlend,
+                SamplerState.PointClamp,
+                null,
+                null,
+                null,
+                camera.Transformation);
+
+            player.Sprite.Draw(gameTime, GameRef.SpriteBatch);
+
+            GameRef.SpriteBatch.End();
+        }
+
+        public void SetUpNewGame()
+        {
+            Texture2D tiles = GameRef.Content.Load<Texture2D>(@"Tiles\tileset1");
+            TileSet set = new TileSet(8, 8, 32, 32);
+            set.Texture = tiles;
+
+            TileLayer background = new TileLayer(200, 200);
+            TileLayer edge = new TileLayer(200, 200);
+            TileLayer building = new TileLayer(200, 200);
+            TileLayer decor = new TileLayer(200, 200);
+
+            map = new TileMap(set, background, edge, building, decor, "test-map");
+
+            map.FillEdges();
+            map.FillBuilding();
+            map.FillDecoration();
+
+            ICharacter teacherOne = Character.FromString(GameRef, "Lance,teacherone,WalkDown,teacherone");
+            ICharacter teacherTwo = PCharacter.FromString(GameRef, "Marissa,teachertwo,WalkDown,tearchertwo");
+
+            GameRef.CharacterManager.AddCharacter("teacherone", teacherOne);
+            GameRef.CharacterManager.AddCharacter("teachertwo", teacherTwo);
+
+            map.Characters.Add("teacherone", new Point(0, 4));
+            map.Characters.Add("teachertwo", new Point(4, 0));
+
+            camera = new Camera();
+        }
+
+        public void LoadExistingGame()
+        {
+        }
+
+        public void StartGame()
+        {
+        }
+    }
+}
